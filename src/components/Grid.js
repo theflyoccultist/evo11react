ba3fb0237bdb90ae233a763e1e243e272dfcfcce
theflyoccultist <y.delahaije@gmail.com>
2024-06-07 11:08:52 +0200

    checklist complete

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,12 +4,30 @@ import './Grid.css';
 const Grid = () => {
     return (
         <div className='grid-container'>
-            <div className='grid-item'>Exfoliate Face</div>
-            <div className='grid-item'>Hydrate Face</div>
-            <div className='grid-item'>Voice Training</div>
-            <div className='grid-item'>Makeup</div>
-            <div className='grid-item'>Solar Cream</div>
-            <div className='grid-item'>Perfume</div>
+            <div className='grid-item'>
+                <input type='checkbox' id='item1'/>
+                <label htmlFor='item1'>Exfoliate Face</label>
+            </div>
+            <div className='grid-item'>
+                <input type='checkbox' id='item2'/>
+                <label htmlFor='item2'>Hydrate Face</label>
+            </div>
+            <div className='grid-item'>
+                <input type='checkbox' id='item3'/>
+                <label htmlFor='item3'>Voice Training</label>
+            </div>
+            <div className='grid-item'>
+                <input type='checkbox' id='item4'/>
+                <label htmlFor='item4'>Makeup</label>
+            </div>
+            <div className='grid-item'>
+                <input type='checkbox' id='item5'/>
+                <label htmlFor='item5'>Solar Cream</label>
+            </div>
+            <div className='grid-item'>
+                <input type='checkbox' id='item6'/>
+                <label htmlFor='item6'>Perfume</label>
+            </div>
         </div>
     );
 };
