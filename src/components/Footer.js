2deac1e9e76ed5428ea1fb38552a75516cf658ff
theflyoccultist <y.delahaije@gmail.com>
2024-06-08 00:50:28 +1000

    react time

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <footer>
+            <h1>Add or share</h1>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Grid.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Grid.css';
+
+const Grid = () => {
+    return (
+        <div className='grid-container'>
+            <div className='grid-item'>Exfoliate Face</div>
+            <div className='grid-item'>Hydrate Face</div>
+            <div className='grid-item'>Voice Training</div>
+            <div className='grid-item'>Makeup</div>
+            <div className='grid-item'>Solar Cream</div>
+            <div className='grid-item'>Perfume</div>
+        </div>
+    );
+};
+
+export default Grid;
